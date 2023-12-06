8b46ad9c125991ce7e28d21b35e4ba80bbd37952
Ryan Yu <69830650+Ryannn41@users.noreply.github.com>
2023-12-06 16:55:47 +0800

    新增表单功能实现

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -3,18 +3,24 @@ import axios from "axios";
 
 const billStore = createSlice({
     name: 'bill',
+    // 数据状态state
     initialState: {
         billList: []
     },
     reducers: {
+        // 同步修改方法
         setBillList(state, action) {
             state.billList = action.payload
+        },
+        // 同步添加账单方法
+        addBill(state, action) {
+            state.billList.push(action.payload)
         }
     }
 })
 
 // 解构 axtionCreater 函数
-const { setBillList } = billStore.actions
+const { setBillList, addBill } = billStore.actions
 
 // 异步编写
 const getBillList = () => {
@@ -26,7 +32,16 @@ const getBillList = () => {
     }
 }
 
-export { getBillList }
+const addBillList = (data) => {
+    return async (dispatch) => {
+        // 异步请求
+        const res = await axios.post('http://localhost:8888/ka', data)
+        // 触发同步 reducer
+        dispatch(addBill(res.data))
+    }
+}
+
+export { getBillList, addBillList }
 
 // 导出 reducer
 const reducer = billStore.reducer
