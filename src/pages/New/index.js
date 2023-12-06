d87b7d8394f29b235f9ea43878b4ff0d0cc86440
Ryan Yu <69830650+Ryannn41@users.noreply.github.com>
2023-12-06 17:05:34 +0800

    时间戳功能

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { addBillList } from '@/store/modules/billStore'
 import { useDispatch } from 'react-redux'
+import dayjs from 'dayjs'
 
 const New = () => {
     const navigate = useNavigate()
@@ -29,11 +30,24 @@ const New = () => {
         const data = {
             type: billType,
             money: billType === 'pay' ? -money : +money,
-            date: new Date(),
+            date: date,
             useFor: useFor
         }
         dispatch(addBillList(data))
     }
+
+    // 存储选择的时间
+    const [date, setDate] = useState()
+
+    // 控制时间打开关闭
+    const [dateVisible, setDateVisible] = useState(false)
+
+    // 确认选择时间
+    const dateConfirm = (value) => {
+        setDate(value)
+        setDateVisible(false)
+    }
+
     return (
         <div className="keepAccounts">
             <NavBar className="nav" onBack={() => navigate(-1)}>
@@ -62,11 +76,14 @@ const New = () => {
                     <div className="kaForm">
                         <div className="date">
                             <Icon type="calendar" className="icon" />
-                            <span className="text">{'今天'}</span>
+                            <span className="text" onClick={() => setDateVisible(true)}>{dayjs(date).format('YYYY-MM-DD')}</span>
+                            {/* 时间选择器 */}
                             <DatePicker
                                 className="kaDate"
                                 title="记账日期"
                                 max={new Date()}
+                                visible={dateVisible}
+                                onConfirm={dateConfirm}
                             />
                         </div>
                         <div className="kaInput">
@@ -94,7 +111,7 @@ const New = () => {
                                         <div
                                             className={classNames(
                                                 'item',
-                                                ''
+                                                useFor === item.type ? 'selected' : ''
                                             )}
                                             key={item.type}
                                             onClick={() => setUseFor(item.type)}
